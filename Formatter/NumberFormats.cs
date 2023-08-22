7c14fd1e3f547edc6e5c4d62007453f2d77e1b08
Nevo <78344410+NotArceuid@users.noreply.github.com>
2023-08-22 19:07:28 +0800

    Update NumberFormats.cs

--- Formatter/NumberFormats.cs
+++ Formatter/NumberFormats.cs
@@ -4,18 +4,20 @@ namespace NumberFormatter
     {
         public static Dictionary<int, string> Letters = new()
         {
-            { 0, "m" },
-            { 1, "b" },
-            { 3, "t" },
-            { 4, "qd" },
-            { 5, "QN" },
-            { 6, "sx" },
-            { 7, "Sp" },
-            { 8, "O" },
-            { 9, "N" },
-            { 10, "de" },
-            { 11, "Ud" },
-            { 12, "DD" }
+            { 0, "" },
+            { 1, "k" },
+            { 2, "m" },
+            { 3, "b" },
+            { 4, "t" },
+            { 5, "qd" },
+            { 6, "QN" },
+            { 7, "sx" },
+            { 8, "Sp" },
+            { 9, "O" },
+            { 10, "N" },
+            { 11, "de" },
+            { 12, "Ud" },
+            { 13, "DD" }
         };
     };
 }
